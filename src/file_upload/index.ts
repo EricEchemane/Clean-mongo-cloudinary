d64243c431e1b61990f1d705fd4d7a7f3a575df5
Eric Echemane <eechemane29@gmail.com>
2022-08-07 10:16:04 +0800

    ImageUpload

--- src/file_upload/index.ts
+++ src/file_upload/index.ts
@@ -6,3 +6,13 @@ cloudinary.config({
     api_key: environment.API_KEY,
     api_secret: environment.API_SECRET,
 });
+
+export class ImageUpload {
+
+    static async upload(base64Encoded: string) {
+        const uploadResponse = await cloudinary.uploader.upload(base64Encoded, {
+            folder: 'images',
+        });
+        return uploadResponse;
+    }
+}
