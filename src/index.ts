f13c04e8f759d1c31c2d750c0120fb2fa4ec1df4
Eric Echemane <eechemane29@gmail.com>
2022-08-07 10:02:16 +0800

    set express json limit to 50mb

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { socketConnectionHandler } from './socket';
 export const app = express();
 
 // middlewares
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors({ origin: "*" }));
 app.use(makeSession(environment.secret));
 app.use(cookieParser());
