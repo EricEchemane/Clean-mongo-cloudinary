b2df8d0a8473dce23acad277598a521f63663e49
Eric Echemane <eechemane29@gmail.com>
2022-08-08 12:07:47 +0800

    new route - POST /user/login

--- src/__clean__/@api/controllers/user.controllers.ts
+++ src/__clean__/@api/controllers/user.controllers.ts
@@ -1,6 +1,9 @@
 import CreateUserUseCase from '@usecases/create-user.usecase';
-import { Request } from 'express';
+import CreateUserLoginUseCase from '@usecases/user/user-login.usecase';
+import { Request, Response } from 'express';
+import environment from 'utils/environment';
 import { RequestError } from 'utils/exceptions';
+import Jwt from 'utils/jwt';
 
 const userControllers = {
     createNewUser: async (req: Request) => {
@@ -11,6 +14,20 @@ const userControllers = {
                 throw new RequestError(400, err.message);
             });
         return newUser;
+    },
+    userLogin: async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const user = await CreateUserLoginUseCase({ email, password })
+            .execute()
+            .catch(err => {
+                res.status(err.code).json(err);
+                return;
+            });
+
+        const token = Jwt.sign(user, environment.secret);
+        const session: any = req.session;
+        session.user = token;
+        res.status(200).end();
     }
 };
 
--- src/__clean__/@api/routes/user.routes.ts
+++ src/__clean__/@api/routes/user.routes.ts
@@ -5,5 +5,6 @@ import userControllers from '@api/controllers/user.controllers';
 const userRoutes = Router();
 
 userRoutes.route('/').post(makeCallback(userControllers.createNewUser, false));
+userRoutes.route('/login').post(userControllers.userLogin);
 
 export default userRoutes;
